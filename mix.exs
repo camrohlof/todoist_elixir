769b94fcf01af757cfcd9ff2300807f23ab9f8fb
camrohlof <camrohlof@gmail.com>
2024-05-16 14:11:26 -0400

    add info for hex package

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule TodoistElixir.MixProject do
   def project do
     [
       app: :todoist_elixir,
+      name: "Todoist Elixir",
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -27,6 +30,19 @@ defmodule TodoistElixir.MixProject do
       {:dotenvy, "~> 0.8.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "A simple Elixir wrapper around the Todoist REST api"
+  end
+
+  defp package() do
+    [
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/camrohlof/todoist_elixir"}
     ]
   end
 end
